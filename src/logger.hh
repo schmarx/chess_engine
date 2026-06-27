a1661b80d46ba2e127eb966d5c6c0cd494f3d6db
Marcus Schmid <marx.schmid1@gmail.com>
2026-06-27 19:13:22 +0200

    added methods for getting date and time strings

--- src/logger.hh
+++ src/logger.hh
@@ -29,19 +29,52 @@ class Logger {
 		printf("%i", val);
 	}
 
-	void timestamp() {
+	void str_pad(char *str, int val) {
+		if (val < 10) {
+			strcat(str, "0");
+		}
+		char buf[4] = "";
+		sprintf(buf, "%i", val);
+		strcat(str, buf);
+	}
+
+	void date_string(char *str) {
 		time_t t = time(NULL);
 		struct tm datetime = *localtime(&t);
 
-		set(FMT_GRAY);
+		str_pad(str, datetime.tm_year + 1900);
+		strcat(str, "-");
+		str_pad(str, datetime.tm_mon + 1);
+		strcat(str, "-");
+		str_pad(str, datetime.tm_mday);
+	}
 
-		printf("[");
-		print_pad(datetime.tm_hour);
-		printf(":");
-		print_pad(datetime.tm_min);
-		printf(":");
-		print_pad(datetime.tm_sec);
-		printf("] ");
+	void time_string(char *str) {
+		time_t t = time(NULL);
+		struct tm datetime = *localtime(&t);
+
+		str_pad(str, datetime.tm_hour);
+		strcat(str, ":");
+		str_pad(str, datetime.tm_min);
+		strcat(str, ":");
+		str_pad(str, datetime.tm_sec);
+	}
+
+	void datetime_string(char *str) {
+		char time_buf[64] = "";
+		char date_buf[64] = "";
+		date_string(date_buf);
+		time_string(time_buf);
+
+		sprintf(str, "%s %s", date_buf, time_buf);
+	}
+
+	void timestamp() {
+		char buf[128] = "";
+		datetime_string(buf);
+
+		set(FMT_GRAY);
+		printf("[%s] ", buf);
 		reset();
 	}
 
